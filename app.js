7f0f512a463d035ec86dacfb3de9f6944e802215
Satilian <viapont@gmail.com>
2019-03-02 14:43:35 +0000

    modified:   README.md 	modified:   app.js 	modified:   package.json

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+const compression = require('compression')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
 
-app.listen(8080, () => console.log(`Example app listening on port ${8080}!`))
-app.use('*', (req, res) => res.send("hello World") )
+const fs = require('fs')
+const https = require('https')
+const options = {
+  cert: fs.readFileSync('./sslcert/fullchain.pem'),
+  key: fs.readFileSync('./sslcert/privkey.pem')
+}
+
+https.createServer(options, app).listen(8008, () =>
+  console.log(`Listening on port 8008!`))
+
+app.use(fileUpload())
+app.use(compression())
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.static('public'))
+app.set('view engine', 'pug')
+app.use('*', (req, res) => res.send("hello World"))
